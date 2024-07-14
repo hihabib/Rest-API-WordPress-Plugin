555fcc058461b587640d9b1109ee4b0a0cbd11fe
Habibul Islam <habibulislam6862@gmail.com>
2024-07-14 14:52:08 +0600

    Plugin's Header is updated

--- index.php
+++ index.php
@@ -1,11 +1,12 @@
 <?php
 /**
- * Plugin Name:        Rest API WP - RAW
- * Description:        Custom Plugin to create Rest API
- * Version:            1.0.0
- * Author:             Habibul Islam
- *  License:           GPL v2 or later
- *  License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Plugin Name:       Rest API WP - RAW
+ * Description:       Custom Plugin to create Rest API
+ * Version:           1.0.0
+ * Author:            Habibul Islam
+ * Author URI:        https://github.com/hihabib
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
  */
 
 // Exit If accessed directly
